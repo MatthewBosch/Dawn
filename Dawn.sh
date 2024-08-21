e1460589cf07216a2a12baed390362d9ab272ed9
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-08-21 23:39:36 +0800

    Update Dawn.sh

--- Dawn.sh
+++ Dawn.sh
@@ -118,11 +118,9 @@ function run_foreign_server_node() {
     # 进入项目目录
     cd Dawn-main || { echo "无法进入 Dawn-main 目录"; exit 1; }
 
-    # 自动替换 main.go 中的指定内容
-    echo "正在更新 main.go 文件..."
     sed -i '/client := resty.New().SetProxy(proxyURL)/s/SetProxy(proxyURL)./SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true}).\n    SetHeader("content-type", "application/json").\n    SetHeader("origin", "chrome-extension:\/\/fpdkjdnhkakefebpekbdhillbhonfjjp").\n    SetHeader("accept", "*\/\*").\n    SetHeader("accept-language", "en-US,en;q=0.9").\n    SetHeader("priority", "u=1, i").\n    SetHeader("sec-fetch-dest", "empty").\n    SetHeader("sec-fetch-mode", "cors").\n    SetHeader("sec-fetch-site", "cross-site").\n    SetHeader("user-agent", "Mozilla\/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/127.0.0.0 Safari\/537.36")/' main.go
 
-    sed -i '/SetHeader("user-agent", "Mozilla\/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/127.0.0.0 Safari\/537.36")/a\
+sed -i '/SetHeader("user-agent", "Mozilla\/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/127.0.0.0 Safari\/537.36")/a\
 if proxyURL != "" {\n    client.SetProxy(proxyURL)\n}' main.go
 
     # 重新编译项目
